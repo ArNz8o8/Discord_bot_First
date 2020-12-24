527ba2f3b5cae6af4cabee385d47195149b7edbe
ArNz|8o8 <adenhartog@me.com>
2020-12-24 12:39:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ if(command === 'version') {
         msg.channel.send('Noice, that loser was banned successfully, bye Felicia');
       } catch (err) {
         console.log(err);
-        msg.channel.send('Cannot execute. Most likely I haz no have permissions or you know, the user was not found');
+        msg.channel.send('Cannot execute. Most likely I haz no permissions or you know, the user was not found');
       }
     }
 })
