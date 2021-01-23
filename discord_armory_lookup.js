307535476b5e11d11ab73b2a3d9ec35474e7d383
ArNz|8o8 <adenhartog@me.com>
2021-01-23 13:53:08 +0100

    Update discord_armory_lookup.js

--- discord_armory_lookup.js
+++ discord_armory_lookup.js
@@ -1,8 +1,6 @@
 // Armory Check for Echelon 808303
 // Coded by ArNz8o8 on jan 21st 2021
-
 const BlizzAPI = require('blizzapi');
-const querystring = require('querystring');
 const Discord = require("discord.js");
 
 module.exports = {
@@ -24,31 +22,7 @@ module.exports = {
 			let argz = args[0].toLowerCase();
 			if (!realm) realm = "darkmoon-faire"
 
-			const Embed = (
-					toonName,
-					toonTitle,
-					toonRealm,
-					toonLevel,
-					toonRace,
-					toonSpec,
-					toonClass,
-					toonGuild,
-					toonIlevel,
-					toonRenown,
-		                    	toonConvenant,
-					toonLastOnline
-				) =>
 
-				new Discord.MessageEmbed()
-				.setColor('#FF8315')
-				.setTitle(`${toonName} ${toonTitle} (${toonRealm})`)
-				.addField(`Currently at:`, `Level ${toonLevel}`)
-				.addField(`${toonName} is a:`, `${toonRace} ${toonSpec} ${toonClass}`)
-				.addField(`Guild:`, `\u200b${toonGuild}`)
-				.addField(`Item level:`, `\u200b${toonIlevel}`, true)
-				.addField(`Renown - Convenant:`, `\u200b${toonRenown} - ${toonConvenant}`, true)
-				.addField(`Last seen:`, `\u200b${toonLastOnline}`)
-				.setFooter('World of Warcraft Armory lookup coded by ArNz8o8', 'https://i.imgur.com/5r8LkNz.png');
 
 			const BnetApi = new BlizzAPI({
 				region: 'eu',
@@ -56,75 +30,123 @@ module.exports = {
 				clientSecret: 'SECRET',
 				refreshExpiredAccessToken: true,
 			});
+			const character_query = await BnetApi.query(`/profile/wow/character/${realm}/${argz}/character-media?namespace=profile-eu&locale=en_GB`).then((data1) => {
+				let picData = data1;
+				let toonAvatar
+				try {
+					toonAvatar = picData.assets[0].value;
+				} catch (error) {
+					toonAvatar = picData.avatar_url;
+				};
 
-			BnetApi.query(`/profile/wow/character/${realm}/${argz}?namespace=profile-eu&locale=en_GB`).then((data) => {
 
-					let apiData = data;
-					let toonName = apiData.name;
-					let toonTitle;
-					try {
-						toonTitle = apiData.active_title.name;
-					} catch (error) {
-						toonTitle = "";
-					};
-					let toonRealm = apiData.realm.name
-					let toonLevel = apiData.level;
-					let toonRace = apiData.race.name;
-					let toonSpec = apiData.active_spec.name;
-					let toonClass = apiData.character_class.name
-					let toonGuild;
-					try {
-						toonGuild = apiData.guild.name;
-					} catch (error) {
-						toonGuild = "Not in any guild";
-					};
-					let toonIlevel = apiData.equipped_item_level
-					let toonRenown;
-					try {
-						toonRenown = apiData.covenant_progress.renown_level;
-					} catch (error) {
-						toonRenown = "N/A";
-					};
-		            		let toonConvenant
-		            		try {
-						toonConvenant = apiData.covenant_progress.chosen_covenant.name;
-					} catch (error) {
-						toonConvenant = "because no convenant";
-					};
-					let toonLastOnline;
-					try {
-						toonLastOnline = apiData.last_login_timestamp;
-					} catch (error) {
-						toonLastOnline = "Unknown";
-					};
-					var d = new Date(toonLastOnline),
-						yyyy = d.getFullYear(),
-						mm = ('0' + (d.getMonth() + 1)).slice(-2),
-						dd = ('0' + d.getDate()).slice(-2),
-						hh = d.getHours(),
-						h = hh,
-						min = ('0' + d.getMinutes()).slice(-2),
-						ampm = 'AM',
-						toonFormattedTime;
+				BnetApi.query(`/profile/wow/character/${realm}/${argz}?namespace=profile-eu&locale=en_GB`).then((data) => {
 
-					if (hh > 12) {
-						h = hh - 12;
-						ampm = 'PM';
-					} else if (hh === 12) {
-						h = 12;
-						ampm = 'PM';
-					} else if (hh == 0) {
-						h = 12;
-					}
-				var toonFormattedTime = dd + '/' + mm + '/' + yyyy + ' at ' + h + ':' + min + ' ' + ampm;
-					message.channel.send(Embed(toonName, toonTitle, toonRealm, toonLevel, toonRace, toonSpec, toonClass, toonGuild, toonIlevel, toonRenown, toonConvenant, toonFormattedTime));
-				})
-				.catch(function(error) {
-					console.log(error);
-				});
+						let apiData = data;
+						let toonName = apiData.name;
+						let toonTitle;
+						try {
+							toonTitle = apiData.active_title.name;
+						} catch (error) {
+							toonTitle = "";
+						};
+						let toonRealm = apiData.realm.name;
+						let toonID = apiData.id;
+						let toonLevel = apiData.level;
+						let toonSex = apiData.gender.name;
+						let toonRace = apiData.race.name;
+						let toonSpec = apiData.active_spec.name;
+						let toonClass = apiData.character_class.name
+						let toonGuild;
+						try {
+							toonGuild = apiData.guild.name;
+						} catch (error) {
+							toonGuild = "Not in any guild";
+						};
+						let toonAchie = apiData.achievement_points
+						let toonIlevel = apiData.equipped_item_level
+						let toonRenown;
+						try {
+							toonRenown = apiData.covenant_progress.renown_level;
+						} catch (error) {
+							toonRenown = "None";
+						};
+						let toonConvenant
+						try {
+							toonConvenant = apiData.covenant_progress.chosen_covenant.name;
+						} catch (error) {
+							toonConvenant = "";
+						};
+						let toonFaction = apiData.faction.name
+						let toonLastOnline;
+						try {
+							toonLastOnline = apiData.last_login_timestamp;
+						} catch (error) {
+							toonLastOnline = "Unknown";
+						};
+						var d = new Date(toonLastOnline),
+							yyyy = d.getFullYear(),
+							mm = ('0' + (d.getMonth() + 1)).slice(-2),
+							dd = ('0' + d.getDate()).slice(-2),
+							hh = d.getHours() + 1,
+							h = hh,
+							min = ('0' + d.getMinutes()).slice(-2),
+							ampm = 'AM',
+							toonFormattedTime;
+
+						if (hh > 12) {
+							h = hh - 12;
+							ampm = 'PM';
+						} else if (hh === 12) {
+							h = 12;
+							ampm = 'PM';
+						} else if (hh == 0) {
+							h = 12;
+						}
+						var toonFormattedTime = dd + '/' + mm + '/' + yyyy + ' at ' + h + ':' + min + ' ' + ampm;
+
+						const Embed = (
+								toonName,
+								toonTitle,
+								toonRealm,
+								toonAvatar,
+								toonLevel,
+								toonSex,
+								toonRace,
+								toonSpec,
+								toonClass,
+								toonGuild,
+								toonAchie,
+								toonIlevel,
+								toonRenown,
+								toonConvenant,
+								toonFaction,
+								toonLastOnline
+							) =>
+
+							new Discord.MessageEmbed()
+							.setColor('#FF8315')
+							.setTitle(`${toonName} ${toonTitle} (${toonRealm})`)
+							.setThumbnail(`${toonAvatar}`)
+							.addField(`Currently at:`, `Level ${toonLevel}`)
+							.addField(`${toonName} is a:`, `${toonSex} ${toonRace} ${toonSpec} ${toonClass}`)
+							.addField(`Guild:`, `\u200b${toonGuild}`, true)
+							.addField(`Achievement points:`, `\u200b${toonAchie}`, true)
+							.addField(`Item level:`, `\u200b${toonIlevel}`, true)
+							.addField(`Renown:`, `\u200b${toonRenown} - (${toonConvenant})`, true)
+							.addField(`Faction:`, `\u200b${toonFaction}`, true)
+							.addField(`Last seen:`, `\u200b${toonLastOnline}`, true)
+							.setFooter('World of Warcraft Armory lookup coded by ArNz8o8', 'https://i.imgur.com/5r8LkNz.png');
+
+						message.channel.send(Embed(toonName, toonTitle, toonRealm, toonAvatar, toonLevel, toonSex, toonRace, toonSpec, toonClass, toonGuild, toonAchie, toonIlevel, toonRenown, toonConvenant, toonFaction, toonFormattedTime));
+					})
+					.catch(error => {
+						message.channel.send(`I am sorry snowflake, \`${argz}\` is either unknown or just misspelled.. feel free to try again`)
+					});
+			})
+
+
+			//console.log(character_query);
 		}
-
-
-		//console.log(character_query);
 	}
 }
